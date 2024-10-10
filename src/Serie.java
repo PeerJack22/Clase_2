0dc8722fe253cf4acf6cef19a2be58541e191ca2
PeerJack22 <pajocina.11@gmail.com>
2024-10-10 08:43:59 -0500

    Agregar nuevas clases (Serie y Videojuego)

---
+++ src/Serie.java
@@ -0,0 +1,23 @@
+public class Serie {
+    //Atributos
+    String nombre;
+    int n_episodios;
+    String director;
+    int calificacion;
+
+    //métodos
+        //Constructor
+    public Serie(String nombre, int n_episodios, String director, int calificacion) {
+        this.nombre = nombre;
+        this.n_episodios = n_episodios;
+        this.director = director;
+        this.calificacion = calificacion;
+    }
+
+    public void imprimir() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Número de episodios: " + n_episodios);
+        System.out.println("Director: " + director);
+        System.out.println("Calificacion: " + calificacion);
+    }
+}
---
+++ src/Videojuego.java
@@ -0,0 +1,23 @@
+public class Videojuego {
+    //Atributos
+    String nombre;
+    String genero;
+    int anio;
+    int calificacion;
+
+    //Métodos
+        //Constructor
+    public Videojuego(String nombre, String genero, int anio, int calificacion) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.anio = anio;
+        this.calificacion = calificacion;
+    }
+
+    public void imprimir() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Genero: " + genero);
+        System.out.println("Año: " + anio);
+        System.out.println("Calificacion: " + calificacion);
+    }
+}
